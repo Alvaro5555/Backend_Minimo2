894c7c94303bbd8fb7a4a0a7f12d2d7e44de4aff
Alvaro5555 <alvaro.cendoya@estudiantat.upc.edu>
2023-01-12 14:16:38 +0100

    1

---
+++ src/api/User.ts
@@ -0,0 +1,19 @@
+import userController from '../controller/userController';
+import { Router } from 'express';
+import { verifyToken, isOwner, istheUser } from '../middlewares/authJWT'
+
+const router = Router();
+
+router.post('/', userController.register); // OK
+router.get('/:id', [verifyToken], userController.profile); // OK
+router.get('/', userController.getall);  // OK
+router.put('/changepass', userController.changePass); // OK
+router.put('/updateName', [verifyToken], userController.updateName); // OK
+router.put('/updateEmail', [verifyToken], userController.updateEmail); // OK
+router.put('/activate', userController.activate); // OK
+router.delete('/', [verifyToken], userController.deleteUser); // OK
+router.put('/checkemail', userController.checkemail); // OK
+router.put('/addFav',[verifyToken], userController.AddtomyFavorites);// OK
+router.put('/delFav',[verifyToken], userController.cancelMyFavorite);
+
+export default router;
